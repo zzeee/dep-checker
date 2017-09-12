9c69396e17e77da52a4f72b8cb432717164af414
zzeee <zzeeee@gmail.com>
2017-09-12 15:06:29 +0300

    forming table

--- examples/index.jsx
+++ examples/index.jsx
@@ -9,6 +9,38 @@ app.use(bodyParser.urlencoded());
 //app.use(bodyParser.urlencoded({ extended: false }))
 
 
+function getDepComp(packstr)
+{
+    return new Promise((okres, rej) => {
+        try
+        {
+        let rdata = dchecker.getLibListFromJson(reqdat);
+        let ininc=0;
+        for (x in rdata) ininc++;
+        let alllib = [];
+        let countlib = 0;
+        let res="";
+        res+="<table border=\"1\">";
+        for (x in rdata) {
+            let qline = rdata[x];
+            let lline = x;
+            console.log(x);
+            let rt = new dchecker.LibChecker(x).then((e) => {
+                res+="<tr><td>" + lline + "</td><td>" + qline + "</td><td>" + e["latest"] + "</td></tr>";
+                countlib++;
+                if (countlib == ininc) {
+                    res+="</table>";
+
+                    okres(res);
+                }
+            }, rej);
+        }
+    }
+    catch (ex){rej(ex)}
+    });
+}
+
+
 
 app.get('/', function (req, res) {
     res.send('<h1>Запостите package.json</h1><form method="POST" enctype="multipart-form/data" action="/"><textarea cols="40" rows="30" name="nam"></textarea><br /><button  type="submit" >Отправить</button></form>');
@@ -18,26 +50,9 @@ app.post('/', function (req, res) {
     let ="";
     reqdat=req.body.nam;
     let ininc=0;
-    let rdata=dchecker.getLibListFromJson(reqdat);
-    for(x in rdata) ininc++;
-    let alllib=[];
-    let countlib=0;
-    res.write("<table border=\"1\">");
-    for (x in rdata)
-    {
-        let qline=rdata[x];
-        let lline=x;
-        console.log(x);
-        let rt=new dchecker.LibChecker(x).then((e)=>{
-            res.write("<tr><td>"+lline+"</td><td>"+qline+"</td><td>"+e["latest"]+"</td></tr>")
-            countlib++;
-            if (countlib==ininc) {
-                res.write("</table>");
-                res.end();
-            }
-        },console.log);
-    }
+    getDepComp(reqdat).then((e)=>res.send(e), console.log);
 });
+
 
 let port=3000;
 app.listen(port, function () {
